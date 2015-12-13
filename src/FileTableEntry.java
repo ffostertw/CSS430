16d2ab4a8835855c965d753e4fa970d3724e8663
ffostertw <kevin@kevintekiMacBook-Pro.local>
2015-12-12 16:17:25 -0800

    FileTableEntry

--- src/FileTableEntry.java
+++ src/FileTableEntry.java
@@ -1,3 +1,19 @@
+public class FileTableEntry{
+    public int seekPtr;
+    public final Inode inode;
+    public final short iNum;
+    public int count;
+    public final String mode;
 
-public class FileTableEntry {
+    public FileTableEntry(Inode i, short iNumber, String mo){
+        seekPtr = 0;
+        inode = i;
+        iNum = iNumber;
+        count = 1;
+        mode = mo;
+        if(mode.compareToIgnoreCase("a") == 0){
+            seekPtr = inode.length;
+        }
+    }
+
 }
