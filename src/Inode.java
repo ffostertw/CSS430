a3c511e245485227d03ec1818aec5ab3a177d571
candyhero <wchan008@uw.edu>
2015-12-12 16:29:15 -0800

    Super block almost finished; Inode minor bug fixes
    Super block:
    - Almost fully implemented, not reviewed and tested yet
    
    Inode block:
    - Now sets correct inode id
    - Renamed some constant integers

--- src/Inode.java
+++ src/Inode.java
@@ -1,12 +1,14 @@
 ////////need to test line 45
-public class Inode{
-    private final static int iNodeSize = 32;
-    private final static int directSize = 11;
+public class Inode
+{
+    private final int INODES_PER_BLOCK = 16;
+    private final int INODE_SIZE = 32;
+    private final int DIRECT_SIZE = 11;
 
     public int length;
     public short count;
     public short flag;
-    public short direct[] = new short[directSize];
+    public short direct[] = new short[DIRECT_SIZE];
     public short indirect;
 
     public Inode()
@@ -16,7 +18,7 @@ public class Inode{
         count = 0;
         flag = 1;
 
-        for(int i = 0; i< directSize ; i++){
+        for(int i = 0; i < DIRECT_SIZE ; i++){
             direct[i] = -1;
         }
         indirect = -1;
@@ -25,14 +27,14 @@ public class Inode{
     public Inode(short iNumber){
 
         // Get the block number and
-        int bNumber = ((int)Math.ceil(iNumber/16.0));//roundup
+        int bNumber = iNumber/INODES_PER_BLOCK;
 
         // Retrive inode data
-        byte[]info = new byte[Disk.blockSize];
+        byte[] info = new byte[Disk.blockSize];
         SysLib.rawread(bNumber,info);
 
         //locate ith inode in the block, int=4 bytes, short = 2bytes,
-        int offset = ((iNumber % 16) * iNodeSize);
+        int offset = ((iNumber % INODES_PER_BLOCK) * INODE_SIZE);
 
         //set all variables from the offset
         length = SysLib.bytes2int(info, offset);
@@ -45,7 +47,7 @@ public class Inode{
         offset += 2;
 
         //set the direct[]
-        for(int i = 0; i < directSize; i++)
+        for(int i = 0; i < DIRECT_SIZE; i++)
         {
             direct[i]= SysLib.bytes2short(info, offset);
             offset += 2;
@@ -59,9 +61,9 @@ public class Inode{
     // Save ith inode to disk
     public void toDisk(short iNumber)
     {
-        int bNumber = ((int)Math.ceil(iNumber/16.0));//roundup
+        int bNumber = iNumber/INODES_PER_BLOCK;
         int offset = 0;
-        byte[]info = new byte[iNodeSize];
+        byte[] info = new byte[INODE_SIZE];
 
         // Write data to template btye block
         SysLib.int2bytes(length, info, offset);
@@ -73,7 +75,7 @@ public class Inode{
         SysLib.short2bytes(flag, info, offset);
         offset+= 2;
 
-        for(int i = 0; i < directSize; i++)
+        for(int i = 0; i < DIRECT_SIZE; i++)
         {
             SysLib.short2bytes(direct[i], info, offset);
             offset += 2;
@@ -83,7 +85,7 @@ public class Inode{
         // Merge new data in this inode with the data in block
         byte[] newInfo = new byte[Disk.blockSize];
         SysLib.rawread(bNumber,newInfo);
-        System.arraycopy(info, 0, newInfo, (((iNumber % 16) * iNodeSize)), iNodeSize);
+        System.arraycopy(info, 0, newInfo, (((iNumber % INODES_PER_BLOCK) * INODE_SIZE)), INODE_SIZE);
 
         // Write back to block
         SysLib.rawwrite(bNumber, newInfo);
--- src/Superblock.java
+++ src/Superblock.java
@@ -1,11 +1,117 @@
 
-public class Superblock {
+public class Superblock
+{
+    private final int DEFAULT_TOTAL_INODES = 64;
+    private final int INODE_SIZE = 32;
     public int totalBlocks; // the number of disk blocks
     public int totalInodes; // the number of inodes
     public int freeList;    // the block number of the free list's head
 
-    public SuperBlock( int diskSize ) {
-        ...
+    public SuperBlock(int diskSize)
+    {
+        // Retrive SuperBlcok data
+        byte[] info = new byte[Disk.blockSize];
+        SysLib.rawread(0,info);
+
+        //set all variables from the offset
+        totalBlocks = diskSize;
+
+        int offset = 4;
+
+        totalInodes = SysLib.bytes2int(info, offset);
+        offset += 4;
+
+        freeList = SysLib.bytes2int(info, offset);
+        offset += 4;
+
+        if (totalInodes <= 0 || freeList < 2)
+        {
+
+
+        }
     }
-    ...
+    public void format()
+    {
+        byte[] info = new byte[Disk.blockSize];
+
+        // Format inodes
+        totalInodes = DEFAULT_TOTAL_INODES;
+        for (int i = 0; i < totalInodes; ++i)
+        {
+            // Reset inode in disk
+            Inode inode = new Inode();
+            inode.flag = 0;
+            inode.toDisk((short)i);
+        }
+
+        // Format free blocks
+        for (freeList = 2 + ((totalInodes - 1) * INODE_SIZE / Disk.blockSize)
+             ;freeList < totalBlocks; ++freeList)
+        {
+            // Reset all data in the block
+            info = new byte[Disk.blockSize];
+            for (int i = 0; i < Disk.blockSize; ++i)
+            {
+                info[i] = 0;
+            }
+
+            // Write pointer to next block
+            SysLib.int2bytes(freeList + 1, info, 0);
+
+            // Finish formatting
+            SysLib.rawwrite(freeList, info);
+        }
+
+        sync();
+    }
+
+    // Update the variables in super block
+    public void sync()
+    {
+        byte[] info = new byte[Disk.blockSize];
+
+        // Update the variables
+        SysLib.int2bytes(totalBlocks, info, 0);
+        SysLib.int2bytes(totalInodes, info, 4);
+        SysLib.int2bytes(freeList, info, 8);
+
+        // Write back to super block
+        SysLib.rawwrite(0, info);
+    }
+
+    public int getFreeBlock()
+    {
+        int n = freeList;
+        if (n != -1)
+        {
+            byte[] info = new byte[Disk.blockSize];
+            SysLib.rawread(n, info);
+
+            // Read the pointer
+            freeList = SysLib.bytes2int(info, 0);
+
+
+            SysLib.int2bytes(0, info, 0);
+            SysLib.rawwrite(n, info);
+        }
+        return n;
+    }
+
+    public boolean returnBlock(int n)
+    {
+        if (n < 0) return false;
+
+        byte[] info = new byte[Disk.blockSize];
+
+        // Initialize block
+        for (int i = 0; i < Disk.blockSize; ++i)
+        {
+            info[i] = 0;
+        }
+
+        SysLib.int2bytes(freeList, info, 0);
+        SysLib.rawwrite(n, info);
+        freeList = n;
+        return true;
+    }
 }
